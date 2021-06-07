218f87780778d87027c20a50f6a136a06173c168
dhauskk <dhassick@gmail.com>
2021-06-07 14:12:39 -0400

    changed wording and spacing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,8 +5,12 @@ function Landing() {
         <div className="Landing">
           <h1>Oil Change</h1>
             <p>If you have ever forgotten when you changed the oil, simply ran out of room or lost your
-              maintenance log book. Dont worry you are not alone, and fortunately there is Oil Change, an
-              app that will be the maintenance log book in your pocket. All you need to do. Just pick out
+              maintenance log book. Don't worry you are not alone, and fortunately there is Oil Change, an
+              app that will be the maintenance log book in your pocket. 
+              
+              </p>
+              <p>
+              Just pick out
               or add your  car, and we will pull up the last time you did any service to your car. Here
               you can view, add, edit, or delete your maintenance logs. We also show you charts of your
               data to give a time line of when you last did your maintenance and the frequency. It is simple
