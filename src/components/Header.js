ac54acc5dbe75a4a41b0ea36c90afe86f265ac74
dhauskk <dhassick@gmail.com>
2021-11-02 21:42:38 -0400

    fixed sign in function toggle call

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
 import './HeaderStyle.css';
 import { Link } from 'react-router-dom';
 
-
 function NavHeader(props) {
     const signIn = props.loggedIn
     const signInButton = (signIn)=> {
-        if(signIn){
-            return <h3>LoggedIn</h3>
+        console.log(signIn)
+        if(signIn === false){
+            return <button>Sign or log in here</button>
         }else {
-            return <h3>Sign Up or Login Here</h3>
+            return <h5>Logged In</h5>  
         }
-    }; 
-    
+    }; //displays sign in button or if the user is logged in on header
     
     return (    
         < header className="App-header" >
             <div className="title-div">
                 <h1 className="title">Oil Change</h1>
                 <p className="app-intro">When did I do that again...</p>
-                <p>{signIn} </p>
             </div>
             <nav className="header-nav" >
                 <Link to="/">
                     <button className="nav-button" >Home</button>
                 </Link>
-                <Link to="/Vehicles">
-                    <button className="nav-button" >Vehicles</button>
+                <Link to="/Profile">
+                    <button className="nav.button">My Profile</button>
+                </Link>
+                {/* <Link to="/Vehicles">
+                    <button className="nav-button" >My Vehicles</button>
                 </Link>
                 <Link to="/About">
                     <button className="nav-button" >About</button>
-                </Link>
+                </Link> */}
             </nav>
-            {signInButton}
+            {signInButton(signIn)} 
         </header >
     );
 }
