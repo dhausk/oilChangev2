db9d2e93b150c082669bb776cab7f937a66cbc5b
dhauskk <dhassick@gmail.com>
2021-11-15 23:37:47 -0500

    new routes & names for pages, new plan comments

--- src/Router.js
+++ src/Router.js
@@ -5,38 +5,35 @@ import Landing from './pages/Landing';
 import Profile from './pages/Profile';
 import VehicleLog from './pages/VehicleLogs';
 
-
-
 const routes = {
-    Landing : "/", //landing and splash page, step two show screenshots
-    Profile : "/:user", //user landing page shows vehicles, crud capable to add vehicles
-    VehicleLog: "/:user/vehicles", //shows vehicle logs and charts, crud capable to add logs
+	Landing: "/", //landing and splash page, step two show screenshots
+	Profile: "/", //user landing page shows user details
+	VehicleLog: "/vehicles", //shows user's vehicles crud capable to add
 }
 
 function AppRouter() {
-    const signedIn = true;
+	const signedIn = true;
 
-    if (signedIn === true) { // signed in Users see this
-        return (
-            <Router>
-                <NavHeader loggedIn={true} />
-                <Route path={routes.Landing} component={Landing}/>
-                <Route path={routes.Profile} component={VehicleLog}/>
-                <Route path={routes.VehicleLog}  />
-                {/* vehicles are shown in the users profile page. vehicle logs display once a vehicle is clicked.
-                each page is crud capable for vehicles and logs.*/}
+	if (signedIn === true) { // signed in Users see this
+		return (
+			<Router>
+				<NavHeader loggedIn={true} />
+				<Route path={routes.Profile} component={Profile} />
+				<Route path={routes.VehicleLog} component={VehicleLog} />
+				{/* vehicles are shown in the users profile page. the vehicle Log page displays once a vehicle is clicked.
+				each page is crud capable for vehicles in profile and logs in vehicleLog.*/}
 
-            </Router>
-        );
-    }   // base landing page for users not signed in.
-    else {
-        return (
-            <Router>
-                <NavHeader loggedIn={false} />
-                <Route path={routes.Landing} component={Landing} />
-            </Router>
-        )
-    }
+			</Router>
+		);
+	}   // base landing page for users not signed in.
+	else {
+		return (
+			<Router>
+				<NavHeader loggedIn={false} />
+				<Route path={routes.Landing} component={Landing} />
+			</Router>
+		)
+	}
 }
 
 export default AppRouter;
